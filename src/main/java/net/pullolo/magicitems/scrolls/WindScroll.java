074603a6f1cde2d34c7f96bb1f2e018f387830d8
TremxNeo <67651877+TremxNeo@users.noreply.github.com>
2025-08-26 02:13:07 +0800

    Update WindScroll.java
    adds configurable velocity

--- src/main/java/net/pullolo/magicitems/scrolls/WindScroll.java
+++ src/main/java/net/pullolo/magicitems/scrolls/WindScroll.java
@@ -1,30 +1,36 @@
 package net.pullolo.magicitems.scrolls;
 
 import net.pullolo.magicitems.utils.CooldownApi;
+import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-public class WindScroll extends Scroll{
+public class WindScroll extends Scroll {
 
-    public WindScroll(){
+    private double velocityMultiplier;
+
+    public WindScroll() {
         type = "wind";
         name = "Cloud Boost";
-        description.add("Get a small dash by bouncing of clouds.");
-        cooldown=8;
+        description.add("Get a small dash by bouncing off clouds.");
+        cooldown = 8; // still hardcoded
+
+        var plugin = Bukkit.getPluginManager().getPlugin("MagicItems"); // Replace if needed
+        var config = plugin.getConfig();
+
+        velocityMultiplier = config.getDouble("wind_scroll.velocity_multiplier", 1.5);
     }
 
     @Override
     public void executeAbility(Player p) {
-        if (CooldownApi.isOnCooldown("wind-scroll", p)){
+        if (CooldownApi.isOnCooldown("wind-scroll", p)) {
             return;
         }
         p.getWorld().spawnParticle(Particle.CLOUD, p.getLocation().clone(), 20, 0.1, 0, 0.1, 0.1);
         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_WIND_CHARGE_WIND_BURST, 1, 1.2f);
-        p.setVelocity(p.getLocation().getDirection().clone().normalize().add(new Vector(0, 0.5, 0)).normalize().multiply(1.5));
+        p.setVelocity(p.getLocation().getDirection().clone().normalize().add(new Vector(0, 0.5, 0)).normalize().multiply(velocityMultiplier));
         CooldownApi.addCooldown("wind-scroll", p, cooldown);
     }
 }
